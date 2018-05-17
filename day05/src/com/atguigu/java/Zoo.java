3d118c7f515119881f2775bb0ed0446a65a01216
daxian37 <daxian37@126.com>
2018-05-17 22:15:29 +0800

    day05_05

--- day05/src/com/atguigu/java/Zoo.java
+++ day05/src/com/atguigu/java/Zoo.java
@@ -1,10 +1,12 @@
 package com.atguigu.java;
 
 /**
- * 面向对象思想的落地法则一：
- * 1。设计类，并设计类的成员（成员变量和成员方法）
- * 2。通过类，类创建类的对象（也称作类的实例化）
- *3. 通过"对象.属性"或"对象.方法"来调用，完成相应的功能
+ * 一、面向对象思想的落地法则一：
+ *     1。设计类，并设计类的成员（成员变量和成员方法）
+ *     2。通过类，类创建类的对象（也称作类的实例化）
+ *     3. 通过"对象.属性"或"对象.方法"来调用，完成相应的功能
+ * 二、创建的多个对象，彼此各自拥有一套类的属性。当其中一个对象的属性进行修改时，不会影响的其他对象的值
+ *
  * */
 public class Zoo {
     public static void main(String [] args){
@@ -30,6 +32,16 @@ public class Zoo {
         //在创建一个类的对象
         Animal a2 = new Animal();
         System.out.println(" name: " + a2.name  +  " age: " + a2.age);
+        a2.name = "小花";
+        System.out.println(" name: " + a1.name  +  " age: " + a1.age);
+        System.out.println(" name: " + a2.name  +  " age: " + a2.age);
+
+        //a3不意味着相较于a1重新创建的一个对象，而是a1与a3共用一个实体
+        Animal a3 = a1;
+        System.out.println(" name: " + a3.name  +  " age: " + a3.age);
+        a3.name = "维尼熊";
+        System.out.println(" name: " + a1.name  +  " age: " + a1.age);
+
 
     }
 }
